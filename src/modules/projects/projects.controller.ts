dfa973befbbb15ba932708b6bc6724d6040bcd13
Wendel Tavares <wendeljt17@gmail.com>
2026-06-26 20:10:43 -0300

    feat: Creating CRUD for projects

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { ProjectsService } from './projects.service';
 
-@Controller('projects')
-export class ProjectsController {}
+@Controller({
+    path: 'projects',
+    version: '1',
+})
+export class ProjectsController {
+    constructor(private projectsService: ProjectsService) {}
+
+    @Get()
+    public getAll() {
+        return this.projectsService.getAll();
+    }
+
+    @Get(':id')
+    public get(@Param('id') id: string) {
+        return this.projectsService.get(id);
+    }
+
+    @Post()
+    public create(@Body() data: any) {
+        return this.projectsService.create(data);
+    }
+
+    @Put(':id')
+    public update(@Param('id') id: string, @Body() data: any) {
+        return this.projectsService.update(id, data);
+    }
+
+    @Delete(':id')
+    public delete(@Param('id') id: string) {
+        return this.projectsService.delete(id);
+    }
+}
--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  public getAll() {
+    return [
+      { id: 1, name: 'Project 1' },
+      { id: 2, name: 'Project 2' },
+      { id: 3, name: 'Project 3' },
+    ];
+  }
+
+  public get(id: string) {
+    return { id, name: `Project ${id}` };
+  }
+
+  public create(data: any) {
+    return 'create teste';
+  }
+
+  public update(id: string, data: any) {
+    return 'update teste';
+  }
+
+  public delete(id: string) {
+    return 'delete teste';
+  }
+}
