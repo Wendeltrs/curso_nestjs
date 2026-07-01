00ed66137fb8d4c3a6caa0494e82b492e95e1f2b
Wendel Tavares <wendeljt17@gmail.com>
2026-07-01 16:13:07 -0300

    feat: Data validation with ClassValidator

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,36 +1,36 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
-import { ProjectsService } from './projects.service';
-import { ProjectDTO } from './projects.dto';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post, Put } from '@nestjs/common'
+import { ProjectDTO } from './projects.dto'
+import { ProjectsService } from './projects.service'
 
 @Controller({
-    path: 'projects',
-    version: '1',
+  path: 'projects',
+  version: '1',
 })
 export class ProjectsController {
-    constructor(private projectsService: ProjectsService) {}
+  constructor(private projectsService: ProjectsService) {}
 
-    @Get()
-    public getAll() {
-        return this.projectsService.getAll();
-    }
+  @Get()
+  public getAll() {
+    return this.projectsService.getAll()
+  }
 
-    @Get(':id')
-    public get(@Param('id') id: string) {
-        return this.projectsService.get(id);
-    }
+  @Get(':id')
+  public get(@Param('id', ParseUUIDPipe) id: string) {
+    return this.projectsService.get(id)
+  }
 
-    @Post()
-    public create(@Body() data: ProjectDTO) {
-        return this.projectsService.create(data);
-    }
+  @Post()
+  public create(@Body() data: ProjectDTO) {
+    return this.projectsService.create(data)
+  }
 
-    @Put(':id')
-    public update(@Param('id') id: string, @Body() data: ProjectDTO) {
-        return this.projectsService.update(id, data);
-    }
+  @Put(':id')
+  public update(@Param('id', ParseUUIDPipe) id: string, @Body() data: ProjectDTO) {
+    return this.projectsService.update(id, data)
+  }
 
-    @Delete(':id')
-    public delete(@Param('id') id: string) {
-        return this.projectsService.delete(id);
-    }
+  @Delete(':id')
+  public delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.projectsService.delete(id)
+  }
 }
