eb8f264dbcfd9895fc5805df4b02608aa77c9ad2
Wendel Tavares <wendeljt17@gmail.com>
2026-08-06 16:06:40 -0300

    refactor: another adjusting in project DTOs

--- src/modules/projects/projects.dto.ts
+++ src/modules/projects/projects.dto.ts
@@ -6,7 +6,7 @@ export class ProjectDTO {
   @ApiProperty() id: string
   @ApiProperty() name: string
   @ApiProperty() description: string
-  @ApiProperty({ isArray: true, type: () => TasksDTO}) tasks: TasksDTO[]
+  @ApiProperty({ type: [TasksDTO] }) tasks: TasksDTO[]
   @ApiProperty({ format: 'date-time' }) createdAt: string
   @ApiProperty({ format: 'date-time' }) updatedAt: string
   @ApiProperty({ format: 'date-time' }) deletedAt: string
